5eea2df734600f997e9819245cb24cd730e090e4
Lyndsey Herbert <lhe@amp.co>
2020-01-24 12:18:26 +0000

    UPDATED spacing

--- tasks/gulpicon.js
+++ tasks/gulpicon.js
@@ -2,11 +2,10 @@
 var log = require('fancy-log');
 var Grunticon = require( 'grunticon-lib' );
 
-module.exports = function( files, config ) {
+module.exports = function(files, config) {
   'use strict';
 
-  return function(callback = () => {}) {
-
+  return function(callback) {
     // get the config
     config.logger = {
       verbose: config.verbose ? log.info : () => {},
@@ -17,7 +16,7 @@ module.exports = function( files, config ) {
     // just a quick starting message
     log('Look, it\'s a gulpicon!');
 
-    files = files.filter( function( file ){
+    files = files.filter( function(file){
       return file.match( /png|svg/ );
     });
 
@@ -29,13 +28,13 @@ module.exports = function( files, config ) {
 
     var output = config.dest;
 
-    if( !output || output && output === '' ) {
+    if(!output || output && output === '') {
       log('The destination must be a directory');
       callback(false);
     }
 
     var gicon = new Grunticon(files, config.dest, config);
 
-    gicon.process( callback );
+    gicon.process(callback);
   };
 };
